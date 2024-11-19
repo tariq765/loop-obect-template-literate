0069c13dcbfec6b2dcc1584a330533ee756af4d7
tariq765 <tariqasghar761@gmail.com>
2024-11-19 11:06:08 +0500

    Add files via upload

---
+++ loop & template literate/main.js
@@ -0,0 +1,107 @@
+for (var i = 0; i < 11; i++) {
+    console.log("tariq ".concat(i));
+}
+function add1(num1, num2) {
+    return num1 + num2;
+}
+console.log(add1(67, 89));
+console.log(add1(155, 55));
+console.log(add1(67, 88));
+function firstname2() {
+    console.log("hellow world");
+}
+firstname2();
+var firstname11 = ["tariq", "shariq", "usman", "tayyab", "farooq,zainab"];
+firstname11.push("adnan"); //add last element
+firstname11.pop(); //remove last element
+firstname11.shift(); //remove first element 
+firstname11.unshift("adnan"); //add first element
+firstname11.shift();
+console.log(firstname11);
+var num71 = [1, 2, 3, 4, 5, 6,];
+//map method
+var doubbleNumber1 = num71.map(function (num) { return num * 2; });
+console.log(doubbleNumber1);
+var first_name1 = "hellow world";
+console.log(first_name1);
+for (var i = 1; i < 99; i++) {
+    var a = 6;
+    console.log("6 x ".concat(i, " = ").concat(a * i));
+}
+var fullname = ["tariq", "adnan", "asghar", "ali", "rabia"];
+fullname.pop();
+console.log(fullname);
+var nume1 = [1, 2, 45, 66,];
+for (var i = 1; i < 11; i++) {
+    var a = 19;
+    console.log("19 x ".concat(i, " = ").concat(a * i));
+}
+for (var i = 1; i < 11; i++) {
+    var b = 87;
+    console.log("87 x ".concat(i, " = ").concat(b * i));
+}
+var total_marks = 800;
+var obtainedMarks = 667;
+var percentage = obtainedMarks / total_marks * 100;
+console.log(percentage.toFixed(1) + "%");
+for (var i = 0; i < 10; i++) {
+    console.log("tariq asghar is a very good boy and husband ".concat(i));
+}
+var manwwar = ["tariq", "nadeem", "bashar", "tayyab", "shariq"];
+console.log(manwwar.push);
+function khalid(add1, add2) {
+    return add1 + add2;
+    console.log(khalid(67, 898));
+}
+console.log(khalid(67, 98));
+var firstName = "tariq";
+console.log(firstName);
+var firstname = ["tariq", "asghar", "ali", "tanveer", "shabbir", "mosa", "abdul kalam"];
+firstname.unshift("rabia");
+console.log(firstname);
+var fish;
+fish = 89,
+    fish = "tariq",
+    fish = true,
+    fish = "abdul ghaffar",
+    fish = 77,
+    fish = 76;
+for (var i = 1; i < 11; i++) {
+    var a = 9;
+    console.log("9 x ".concat(i, " = ").concat(a * i));
+}
+var myobject = {
+    name: "tariq",
+    age: 34,
+    address: "b.116 mominabad orangi town karachi pakistan",
+    hobbiess: ["cricket", "hockey", "football", "racket", "computer gaming", "use mobile phone"]
+};
+console.log(myobject.hobbiess);
+var monno;
+(function (monno) {
+    monno[monno["tariq"] = 0] = "tariq";
+    monno[monno["asghar"] = 1] = "asghar";
+    monno[monno["ali"] = 2] = "ali";
+})(monno || (monno = {}));
+console.log(monno.ali);
+function shahbaz(a, b) {
+    return a + b;
+}
+console.log(shahbaz(88, 90));
+var israising = false;
+if (israising) {
+    console.log("wear a sun glass");
+}
+else {
+    console.log("wear a chadda");
+}
+console.log(israising);
+var MyName = ("tariq");
+var Myfathername = ("ali asghar");
+var My_age = 34;
+var MyHomeAddress = ("B.116 mominabad orangi town karachi pakistan");
+console.log("my name is ".concat(MyName, " my father name is ").concat(Myfathername, " my age is ").concat(My_age, " my address is ").concat(MyHomeAddress));
+var value = 77;
+var value2 = 23;
+var fullvalue = value + value2;
+console.log(fullvalue);
---
+++ loop & template literate/main.ts
@@ -0,0 +1,214 @@
+
+ 
+
+
+
+
+
+for(let i=0;i<11;i++){
+    console.log(`tariq ${i}`);
+    
+}
+
+
+
+function add1(num1:number,num2:number):number{
+    return num1+num2
+}
+console.log(add1(67,89));
+console.log(add1(155,55));
+console.log(add1(67,88));
+
+
+function firstname2(){
+    console.log("hellow world");
+    
+}
+firstname2 ()
+
+
+let firstname11:string[]=["tariq","shariq","usman","tayyab","farooq,zainab"];
+firstname11 .push("adnan") //add last element
+firstname11.pop() //remove last element
+firstname11.shift() //remove first element 
+firstname11.unshift ("adnan")//add first element
+
+firstname11.shift()
+
+
+console.log (firstname11)
+
+let num71:number[]=[1,2,3,4,5,6,]
+
+//map method
+ let doubbleNumber1:number[]=num71.map(num =>num*2)
+
+console.log(doubbleNumber1);
+
+
+let first_name1:string="hellow world"
+console.log(first_name1);
+
+for(let i=1;i<99;i++){
+    let a=6
+console.log(`6 x ${i} = ${a*i}`);
+
+}
+
+
+
+
+let fullname:string []=["tariq","adnan","asghar","ali","rabia"];
+fullname.pop ()
+console.log(fullname);
+
+
+
+let nume1:number []=[1,2,45,66,];
+
+for(let i =1;i < 11;i++){
+    let a=19
+    console.log(`19 x ${i} = ${a*i}`);
+}    
+
+
+for (let i =1;i < 11;i++){
+    let b=87
+    console.log(`87 x ${i} = ${b*i}`);
+    
+} 
+
+
+
+let total_marks:number=800;
+let obtainedMarks:number=667;
+let percentage :number=obtainedMarks/total_marks*100
+console.log(percentage.toFixed(1)+`%`);
+
+
+for(let i =0;i < 10;i++){
+    console.log(`tariq asghar is a very good boy and husband ${i}`);
+    
+}
+let manwwar:string[]=["tariq","nadeem","bashar","tayyab","shariq"];
+console.log(manwwar.push);
+
+
+
+
+
+
+
+function khalid(add1:number,add2:number):number{
+
+return add1+add2
+console.log(khalid(67,898));}
+
+
+console.log(khalid(67,98));
+
+let firstName:string="tariq";
+console.log(firstName);
+
+
+
+let firstname:string[]=["tariq","asghar","ali","tanveer","shabbir","mosa","abdul kalam"]
+firstname.unshift("rabia")
+console.log(firstname);
+
+let fish:string|number|boolean
+fish=89,
+fish="tariq",
+fish=true,
+fish="abdul ghaffar",
+fish=77,
+fish=76;
+
+
+for(let i =1;i < 11;i++){
+    let a = 9
+    console.log(`9 x ${i} = ${a*i}`)
+    }
+
+
+let myobject:{
+    name:string,
+    age:number,
+    address:string,
+    hobbiess:string[]
+}={
+    name:"tariq",
+    age:34,
+    address:"b.116 mominabad orangi town karachi pakistan",
+    hobbiess:["cricket","hockey","football","racket","computer gaming","use mobile phone"]
+    
+}
+console.log(myobject.hobbiess);
+
+enum monno{
+    tariq,asghar,ali
+}
+console.log(monno.ali);
+
+
+function shahbaz (a:number,b:number):number{
+    return a+b
+}
+console.log(shahbaz(88,90));
+
+
+let israising:boolean=false;
+if(israising){
+    console.log("wear a sun glass");
+}else{
+    console.log("wear a chadda");
+    
+}
+console.log(israising);
+
+let MyName:string=("tariq");
+let Myfathername:string=("ali asghar");
+let My_age:number=34;
+let MyHomeAddress:string=(`B.116 mominabad orangi town karachi pakistan`);
+console.log(`my name is ${MyName} my father name is ${Myfathername} my age is ${My_age} my address is ${MyHomeAddress}`);
+
+
+let value:number=77;
+let value2:number=23;
+let fullvalue:number=value+value2
+console.log(fullvalue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
